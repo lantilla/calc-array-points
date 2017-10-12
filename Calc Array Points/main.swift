bef3d8c01274c4a31038d929dbda08a1556752c2
lantilla <32445340+lantilla@users.noreply.github.com>
2017-10-12 16:06:36 -0700

    Initial Commit

---
+++ Calc Array Points/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Calc Array Points
+//
+//  Created by Lauren Antilla on 10/12/17.
+//  Copyright © 2017 Lauren Antilla. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
